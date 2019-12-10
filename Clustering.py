a2af0e21249c18360fa07407936c93d7ed9d3f1b
raj09523 <rvardhan09523@gmail.com>
2019-12-10 23:55:00 +0530

    first commit

---
+++ Clustering.py
@@ -0,0 +1,50 @@
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Importing the dataset
+dataset = pd.read_csv("cluster.csv")
+X = dataset.iloc[:, 0:12].values
+
+# y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 4, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+from sklearn.decomposition import PCA
+reduced_data = PCA(n_components=2).fit_transform(X)
+results = pd.DataFrame(reduced_data,columns=['pca1','pca2'])
+
+sns.scatterplot(x='pca1', y='pca2', hue=y_kmeans, data=results)
+plt.title('K-means Clustering with 2 dimensions')
+plt.show()
